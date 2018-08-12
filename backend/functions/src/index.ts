7bcad307f032e0eb414640b1d82fc0225b59a914
Itzik Bucktshin <Itzik.Bucktshin@nice.com>
2018-08-12 11:06:23 +0300

    backenb - send email meassegs from node js / firebase function

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,5 +1,26 @@
 import * as functions from 'firebase-functions';
+var nodemailer = require('nodemailer');
 
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'shop.mailbox.dev@gmail.com',
+      pass: 'AdminAdmin'
+    }
+  });
+var mailOptions = {
+    from: 'shop.mailbox.dev@gmail.com',
+    to: 'shop.mailbox.dev@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+};
+transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+});
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
